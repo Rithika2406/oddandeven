407f63b166948ed3605e1bab1be5f05e37181063
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-16 18:13:42 +0530

    Add files via upload

---
+++ oddandeven.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int A = 15;
+    int C = 17;
+
+    if (A% 2 == 0 && C % 2 == 0)
+    {
+        printf("Both A and C are even\n");
+    }
+    else if (A % 2!= 0 && C % 2 != 0)
+    {
+        printf("Both A and C are odd\n");
+    }
+    else
+    {
+        printf("A is odd and C is even or vice versa\n");
+    }
+
+    A = 15;
+    C = 18;
+
+    if (A % 2== 0 && C % 2 == 0)
+    {
+        printf("Both A and C are even\n");
+    }
+    else if (A % 2!= 0 && C % 2 !=0)
+    {
+        printf("Both A and C are odd\n");
+    }
+    else
+    {
+        printf("A is odd and C is even or vice versa\n");
+    }
+
+    return 0;
+}
